c4b040f58e42be598fe594acf391a659f834504a
hoadang <quan.nguyen@kbb.com>
2017-11-25 23:23:24 +0700

    correct use Department

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\department;
+use App\Department;
 
 class DepartmentController extends Controller
 {
