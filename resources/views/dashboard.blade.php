d90c751f8958fc20d3a66ce93c34888f8e03b762
franklinokech <franklinokech@gmail.com>
2018-06-09 14:42:37 +0300

    Changed the background color

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,6 +31,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
+  <style type="text/css">
+    body{
+      background-color: blue;
+    }
+  </style>
 </head>
 <!--
 BODY TAG OPTIONS:
